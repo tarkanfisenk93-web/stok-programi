7a48e0b8dcacb2b2764719d6f8ac0a1d3243b1d4
tarkanfisenk93-web <tarkanfisenk93@gmail.com>
2026-08-19 00:54:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
-// Supabase bilgilerini buraya yaz.
-const SUPABASE_URL = "BURAYA_SUPABASE_PROJECT_URL";
-const SUPABASE_KEY = "BURAYA_SUPABASE_PUBLISHABLE_KEY"; // Service role kullanma!
-window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Supabase bağlantısı
+const SUPABASE_URL = "https://urxbtzborgopjeqwqfu.supabase.co";
+
+const SUPABASE_KEY = sb_publishable_eWD_FzCyLR6UitfER7EXbg_TAP9nVXD
+
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);

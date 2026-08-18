09af0d06b1008aadd2001d2922c743d13c33824e
tarkanfisenk93-web <tarkanfisenk93@gmail.com>
2026-08-19 01:12:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 // Supabase bağlantısı
+
 const SUPABASE_URL = "https://urxbtzborgopjeqwqfu.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_eWD_FzCyLR6UitfER7EXbg_TAP9nVXD";
 
-window.supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+window.supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );

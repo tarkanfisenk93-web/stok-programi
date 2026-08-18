54d4ae28026b5521e55aa6e5f7ec0c49e1e1284f
tarkanfisenk93-web <tarkanfisenk93@gmail.com>
2026-08-19 01:34:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase bağlantısı
 
-const SUPABASE_URL = "https://urxbtzborgopjeq...supabase.co";
+const SUPABASE_URL = "https://urxbtzborgopjeqwqqfu.supabase.co/rest/v1/";
 
 const SUPABASE_KEY = "sb_publishable_eWD_FzCyLR6UitfER7EXbg_TAP9nVXD";
 
